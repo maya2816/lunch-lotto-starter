e7dbdb9fa0856c5007bd67463ab32e8c761dd93e
Maya Kfir <mayakfir@seas.upenn.edu>
2025-04-16 16:47:39 -0400

    feat: add background service worker for API handling

--- background.js
+++ background.js
@@ -1,12 +1,28 @@
-chrome.alarms.create("dailyReminder", { when: Date.now(), periodInMinutes: 1440 });
+// Listen for messages from popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'fetchRestaurants') {
+    fetch(request.url)
+      .then(response => response.json())
+      .then(data => sendResponse({ success: true, data: data }))
+      .catch(error => sendResponse({ success: false, error: error.message }));
+    return true; // Will respond asynchronously
+  }
+});
 
+// Set up daily reminder (moved after the message listener)
+chrome.alarms.create("dailyReminder", { 
+  when: Date.now(),
+  periodInMinutes: 1440 // 24 hours
+});
+
+// Listen for alarm
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "dailyReminder") {
     chrome.notifications.create("lunchTime", {
       type: "basic",
       iconUrl: "assets/icon.png",
       title: "Lunch Lotto",
-      message: "It's time for lunch! Open the Lunch Lotto extension to find your meal.",
+      message: "It's time for lunch! Open the Lunch Lotto extension to find your meal."
     });
   }
 });
